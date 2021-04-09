ff5941d5dac984913d64c4d0dd20c982969b8995
balbazauras <65706419+balbazauras@users.noreply.github.com>
2021-04-09 13:13:50 +0300

    Initial commit

---
+++ RSAEncryption/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace RSAEncryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.DecryptedText = new System.Windows.Forms.TextBox();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.TextToEncrypt = new System.Windows.Forms.TextBox();
+            this.folderSelectButton = new System.Windows.Forms.Button();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fileNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SelectFileButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.Location = new System.Drawing.Point(394, 166);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(75, 23);
+            this.DecryptButton.TabIndex = 8;
+            this.DecryptButton.Text = "Decrypt";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click_1);
+            // 
+            // DecryptedText
+            // 
+            this.DecryptedText.Location = new System.Drawing.Point(286, 92);
+            this.DecryptedText.Multiline = true;
+            this.DecryptedText.Name = "DecryptedText";
+            this.DecryptedText.Size = new System.Drawing.Size(300, 68);
+            this.DecryptedText.TabIndex = 7;
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Location = new System.Drawing.Point(89, 166);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(75, 23);
+            this.EncryptButton.TabIndex = 6;
+            this.EncryptButton.Text = "Encrypt";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.EncryptButton_Click_1);
+            // 
+            // TextToEncrypt
+            // 
+            this.TextToEncrypt.Location = new System.Drawing.Point(12, 92);
+            this.TextToEncrypt.Multiline = true;
+            this.TextToEncrypt.Name = "TextToEncrypt";
+            this.TextToEncrypt.Size = new System.Drawing.Size(267, 68);
+            this.TextToEncrypt.TabIndex = 5;
+            // 
+            // folderSelectButton
+            // 
+            this.folderSelectButton.Location = new System.Drawing.Point(73, 12);
+            this.folderSelectButton.Name = "folderSelectButton";
+            this.folderSelectButton.Size = new System.Drawing.Size(75, 23);
+            this.folderSelectButton.TabIndex = 14;
+            this.folderSelectButton.Text = "Select folder";
+            this.folderSelectButton.UseVisualStyleBackColor = true;
+            this.folderSelectButton.Click += new System.EventHandler(this.folderSelectButton_Click);
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(70, 38);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(28, 13);
+            this.pathLabel.TabIndex = 15;
+            this.pathLabel.Text = "path";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Path:";
+            // 
+            // fileNameTextBox
+            // 
+            this.fileNameTextBox.Location = new System.Drawing.Point(73, 66);
+            this.fileNameTextBox.Name = "fileNameTextBox";
+            this.fileNameTextBox.Size = new System.Drawing.Size(206, 20);
+            this.fileNameTextBox.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "File name:";
+            // 
+            // SelectFileButton
+            // 
+            this.SelectFileButton.Location = new System.Drawing.Point(394, 12);
+            this.SelectFileButton.Name = "SelectFileButton";
+            this.SelectFileButton.Size = new System.Drawing.Size(75, 23);
+            this.SelectFileButton.TabIndex = 20;
+            this.SelectFileButton.Text = "Select file";
+            this.SelectFileButton.UseVisualStyleBackColor = true;
+            this.SelectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(285, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "File name:";
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.AutoSize = true;
+            this.fileNameLabel.Location = new System.Drawing.Point(346, 66);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(49, 13);
+            this.fileNameLabel.TabIndex = 22;
+            this.fileNameLabel.Text = "file name";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 413);
+            this.Controls.Add(this.fileNameLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SelectFileButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileNameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.folderSelectButton);
+            this.Controls.Add(this.DecryptButton);
+            this.Controls.Add(this.DecryptedText);
+            this.Controls.Add(this.EncryptButton);
+            this.Controls.Add(this.TextToEncrypt);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RSA Algorithm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button DecryptButton;
+        private System.Windows.Forms.TextBox DecryptedText;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.TextBox TextToEncrypt;
+        private System.Windows.Forms.Button folderSelectButton;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox fileNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button SelectFileButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label fileNameLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ RSAEncryption/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace RSAEncryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        static public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(1024))
+                {
+                    RSA.ImportParameters(RSAKey);
+                    encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+        }
+
+        static public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(1024))
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+        }
+
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(1024);
+        byte[] plaintext;
+        byte[] encryptedtext;
+        
+        private void folderSelectButton_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathLabel.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+
+        private void SelectFileButton_Click_1(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileNameLabel.Text = openFileDialog1.FileName;
+
+            }
+        }
+
+        private void EncryptButton_Click_1(object sender, EventArgs e)
+        {
+            UnicodeEncoding ByteConverter = new UnicodeEncoding();
+            plaintext = ByteConverter.GetBytes(TextToEncrypt.Text);
+            encryptedtext = Encryption(plaintext, RSA.ExportParameters(false), false);
+
+
+            string filepath = String.Format(@"{0}\{1}.txt", pathLabel.Text, fileNameTextBox.Text);
+            using (StreamWriter writer = new StreamWriter(filepath))
+            {
+                writer.Write(Convert.ToBase64String(encryptedtext, Base64FormattingOptions.InsertLineBreaks));
+            }
+        }
+
+        private void DecryptButton_Click_1(object sender, EventArgs e)
+        {
+            string asd = File.ReadAllText(fileNameLabel.Text);
+            byte[] src = Convert.FromBase64String(asd);
+
+            byte[] decryptedtex = Decryption(src, RSA.ExportParameters(true), false);
+            DecryptedText.Text = ByteConverter.GetString(decryptedtex);
+        }
+    }
+}
